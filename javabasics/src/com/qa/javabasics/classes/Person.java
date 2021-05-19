2622d6de48741c88b8cef7b36a288f61a23e959b
M-java86 <mpythondata@outlook.com>
2021-05-18 20:42:32 -0400

    MO: modifed Person.java

--- javabasics/src/com/qa/javabasics/classes/Person.java
+++ javabasics/src/com/qa/javabasics/classes/Person.java
@@ -8,5 +8,13 @@ public class Person {
   public String eyecolor;
   public String ethnicity;
   public String gender;
+  public boolean isBreathing;
+  
+  public static void read() {
+	  System.out.println("{name} is reading the book title: {given}");
+  }
+  
   
 }
+
+
