886bac6118cdc7f8e811fc3448836c6cfc2ce63a
M-java86 <mpythondata@outlook.com>
2021-05-18 20:53:53 -0400

    MO: added walkSpeed =30,

--- javabasics/src/com/qa/javabasics/classes/Person.java
+++ javabasics/src/com/qa/javabasics/classes/Person.java
@@ -9,12 +9,29 @@ public class Person {
   public String ethnicity;
   public String gender;
   public boolean isBreathing;
+  public int walkSpeed = 30; // in feet 
+
+
   
   public static void read() {
 	  System.out.println("{name} is reading the book title: {given}");
   }
+  public boolean isStillBreathing() {
+	  return isBreathing;
+  }
+
+  public String speak (String msg) {
+	  return "{name} says: " +msg;
+  }
   
-  
+  public int walk() {
+	  return walkSpeed; 
+  }
 }
+
+  
+  
+  
+ 
 
 
