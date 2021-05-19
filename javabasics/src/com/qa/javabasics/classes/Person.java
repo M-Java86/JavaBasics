bf144c81f54a56a8158ab75b98b5c30e48d23550
M-java86 <mpythondata@outlook.com>
2021-05-18 20:30:48 -0400

    MO: Added classes, String

---
+++ javabasics/src/com/qa/javabasics/classes/Person.java
@@ -0,0 +1,12 @@
+package com.qa.javabasics.classes;
+
+public class Person {
+  
+	public String name;
+  public float height;
+  public int age;
+  public String eyecolor;
+  public String ethnicity;
+  public String gender;
+  
+}
