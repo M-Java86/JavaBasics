696c2ff12d2529f73856e9f9f9ba983675623e5e
M-java86 <mpythondata@outlook.com>
2021-05-18 22:18:15 -0400

    MO: Added newPerson-"Charlotte", 28, "Female"-System.out.println(personVar.gender);

--- javabasics/src/com/qa/javabasics/classes/Runner.java
+++ javabasics/src/com/qa/javabasics/classes/Runner.java
@@ -8,8 +8,9 @@ class Runner {
 		
 		
 		Person bob = new Person("Bob", 32, "male");
+		Person personVar = new Person("Charlotte", 28, "Female");
 		
-		System.out.println(bob.gender);
+		System.out.println(personVar.gender);
 		
 		
 		System.out.println("APP END");
